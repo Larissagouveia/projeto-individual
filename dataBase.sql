377911108b9aa4f86074456c7a4aab0c10aae03c
Larissagouveia <100040183+Larissagouveia@users.noreply.github.com>
2022-06-03 15:40:30 -0300

    Add files via upload

---
+++ dataBase.sql
@@ -0,0 +1,42 @@
+CREATE DATABASE majesty;
+
+
+USE majesty;
+
+CREATE TABLE usuario (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(50),
+	email VARCHAR(50),
+	senha VARCHAR(50)
+);
+
+CREATE TABLE estilo (
+idEstilo INT PRIMARY KEY AUTO_INCREMENT,
+tipo VARCHAR(50)
+) AUTO_INCREMENT = 100;
+
+select * from votacao;
+
+CREATE TABLE votacao (
+fkUsuario int unique,
+foreign key (fkUsuario) references usuario(id),
+fkEstilo int,
+foreign key (fkEstilo) references estilo(idEstilo)
+);
+
+insert into estilo (tipo) values ('Opção1'),
+                                 ('Opção2'),
+                                 ('Opção3'),
+                                 ('Opção4');
+                                 
+
+                                 
+select * from votacao;
+select * from usuario;
+
+
+select count(fkUsuario) as 'Votos', tipo from votacao join estilo on fkEstilo=idEstilo group by tipo;             
+
+
+
+
